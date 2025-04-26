d508c1ac4c74568e46902bd17c8ba46875bd55ce
TomZz <pythongolangkotlinrust@gmail.com>
2025-04-26 11:59:34 +0800

    Update install.sh for my personal use

--- install.sh
+++ install.sh
@@ -11,12 +11,12 @@ case $ARCH in
 esac
 
 # prepare the download URL
-GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/rgwood/systemctl-tui/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/rust-kotlin/systemctl-tui/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 GITHUB_FILE="systemctl-tui-${ARCH}-unknown-linux-musl.tar.gz"
-GITHUB_URL="https://github.com/rgwood/systemctl-tui/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
+GITHUB_URL="https://github.com/rust-kotlin/systemctl-tui/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
 
 # install/update the local binary
 curl -L -o systemctl-tui.tar.gz $GITHUB_URL
-tar xzvf systemctl-tui.tar.gz systemctl-tui
-install -Dm 755 systemctl-tui -t "$DIR"
-rm systemctl-tui systemctl-tui.tar.gz
+tar xzvf systemctl-tui.tar.gz st
+install -Dm 755 st -t "$DIR"
+rm st systemctl-tui.tar.gz
